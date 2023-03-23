ab5be87720d056a3c3b03b108368d6a284f255cd
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-03-23 01:38:11 +0000

    casi hecho, falta solo la duncionalidad del reproductor

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Player from "./player.jsx";
 
+const songURL = "https://assets.breatheco.de/apis/sound/";
+const songObj = [];
 
 const Home = () => {
+	const [name, setName] = useState([])
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/sound/songs")
+		.then(response => response.json())
+		.then(data => {
+			setName(data)
+			for(let i = 0; i < data.length; i++){
+				songObj.push(data[i]);
+			}
+		})
+		console.log(songObj)
+		
+	})
+	
 	return (
 		<div>
-			<div className="songContainer"></div>
+			<div className="songContainer">
+				<ol className="mx-0 mt-0 mb-1 p-0">
+					{name.map((title, i) => 
+					<div className="trackContainer py-2 ps-5">
+						{/* usar la clase trackSelected cuando se este reproduciendo la cancion */}
+						<li className="track" key={i}>{title.name}</li>
+					</div>)}
+				</ol>
+			</div>
 			<Player />
 		</div>
 	);
